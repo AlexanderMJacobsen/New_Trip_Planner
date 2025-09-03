c3d151746e3292eddefd9538dc28898171f1d653
Alexander <alja0004@stud.ek.dk>
2025-09-03 05:41:30 +0200

    fix: minor cleanup

--- src/Trip_P/TripManager.java
+++ src/Trip_P/TripManager.java
@@ -30,50 +30,72 @@ public class TripManager {
 
     public static void createStandardTrip() {
         int newTripID = idTripCreator();
+
         System.out.println("Country Enter (String): ");
         String country = Util.stringScan();
+
         System.out.println("Duration Enter (int): ");
         int duration = Util.intScan();
+
         System.out.println("Hotel stay Enter (True||False): ");
         boolean hotel = Util.booleanScan();
+
         System.out.println("Flight Enter (True||False): ");
         boolean flight = Util.booleanScan();
+
         trips.add(new Trip(newTripID, country, duration, hotel, flight));
+
         System.out.println("Successfully created a Trip");
     }
     public static void createSkiTrip() {
         int newTripID = idTripCreator();
+
         System.out.println("Country Enter (String): ");
         String country = Util.stringScan();
+
         System.out.println("Duration Enter (int): ");
         int duration = Util.intScan();
+
         System.out.println("Hotel stay Enter (True||False): ");
         boolean hotel = Util.booleanScan();
+
         System.out.println("Flight Enter (True||False): ");
         boolean flight = Util.booleanScan();
+
         System.out.println("Warm Drinks Enter (True||False): ");
         boolean warmDrink = Util.booleanScan();
+
         System.out.println("Rent Ski Equipment Enter (True||False): ");
         boolean skiEquipment = Util.booleanScan();
+
         trips.add(new SkiTrip(newTripID, country, duration, hotel, flight, warmDrink, skiEquipment));
+
         System.out.println("Successfully created a SkiTrip");
 
     }
     public static void createBeachTrip() {
         int newTripID = idTripCreator();
+
         System.out.println("Country Enter (String): ");
         String country = Util.stringScan();
+
         System.out.println("Duration Enter (int): ");
         int duration = Util.intScan();
+
         System.out.println("Hotel stay Enter (True||False): ");
         boolean hotel = Util.booleanScan();
+
         System.out.println("Flight Enter (True||False): ");
         boolean flight = Util.booleanScan();
+
         System.out.println("Swimp Equipment Enter (True||False): ");
         boolean swimEquipment = Util.booleanScan();
+
         System.out.println("Sunscreen Enter (True||False): ");
         boolean sunscreen = Util.booleanScan();
+
         trips.add(new BeachTrip(newTripID, country, duration, hotel, flight, swimEquipment, sunscreen));
+
         System.out.println("Successfully created a BeachTrip");
     }
 
@@ -107,7 +129,8 @@ public class TripManager {
                     System.out.println("Set new Warm Drinks (True||False)");
                     ((SkiTrip) trip).setWarmDrink((Util.booleanScan()));
                     ((SkiTrip) trip).setRentingEquipment((Util.booleanScan()));
-                } else if (trip instanceof BeachTrip) {
+                }
+                else if (trip instanceof BeachTrip) {
                     System.out.println("Set new country (string):");
                     trip.setCountry(Util.stringScan());
                     System.out.println("Set new duration (int):");
@@ -131,7 +154,6 @@ public class TripManager {
                     System.out.println("Set new Flight (True||False):");
                     trip.setFlight(Util.booleanScan());
                 }
-//boolean swimEquipment, boolean sunscreen,  boolean warmDrinks, boolean isRentingEquipment
             }
         }
 
